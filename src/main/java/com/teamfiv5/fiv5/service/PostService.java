f43e130cc506fbc459c36308271ab771aa5b94d3
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-11-15 16:35:37 +0900

    fix: fix test config

--- src/main/java/com/teamfiv5/fiv5/service/PostService.java
+++ src/main/java/com/teamfiv5/fiv5/service/PostService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Set; 
+import java.util.Set;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
